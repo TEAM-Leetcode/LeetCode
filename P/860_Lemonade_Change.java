ecabab34136222f5ffe7c4d5fb06b1d3c87f3af4
MrP29 <pokkm29@gmail.com>
2021-07-06 22:41:34 -0400

    860_Upload_Java_P

---
+++ P/860_Lemonade_Change.java
@@ -0,0 +1,25 @@
+//Time: O(N) Space: O(1)
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        for(int i = 0; i < bills.length; i++) {
+            if(bills[i] == 5) {
+                five++;
+            } else if(bills[i] == 10) {
+                five--;
+                ten++;
+            } else {
+                if(ten > 0) {
+                    ten--;
+                    five--;
+                } else {
+                    five -= 3;
+                }
+            }
+            if(five < 0)
+                return false;
+        }
+        return true;
+    }
+}
