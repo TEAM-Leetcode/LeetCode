a9c8371486698f19b7a223e269b9fe79713db024
MrP29 <pokkm29@gmail.com>
2021-07-13 00:14:12 -0400

    171_Upload_Java_P

---
+++ P/171_Excel_Sheet_Column_Number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int column = 0;
+        for(int i = 0; i < columnTitle.length(); i++) {
+            int num = columnTitle.charAt(i) - 'A' + 1;
+            double multiple = Math.pow(26, columnTitle.length() - i - 1);
+            num *= (int) multiple;
+            column += num;
+        }
+        return column;
+    }
+}
+
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int column = 0;
+        for(int i = columnTitle.length() - 1; i >= 0; i--) {
+            int num = columnTitle.charAt(i) - 'A' + 1;
+            double multiple = Math.pow(26, columnTitle.length() - i - 1);
+            num *= (int) multiple;
+            column += num;
+        }
+        return column;
+    }
+}
