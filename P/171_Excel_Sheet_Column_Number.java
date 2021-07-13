e24c91f255970c7b63040cadb01d06085929ac27
MrP29 <pokkm29@gmail.com>
2021-07-13 00:18:06 -0400

    171_Update_Java_P

--- P/171_Excel_Sheet_Column_Number.java
+++ P/171_Excel_Sheet_Column_Number.java
@@ -3,6 +3,17 @@ class Solution {
         int column = 0;
         for(int i = 0; i < columnTitle.length(); i++) {
             int num = columnTitle.charAt(i) - 'A' + 1;
+            column = column * 26 + num;
+        }
+        return column;
+    }
+}
+
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int column = 0;
+        for(int i = 0; i < columnTitle.length(); i++) {
+            int num = columnTitle.charAt(i) - 'A' + 1;
             double multiple = Math.pow(26, columnTitle.length() - i - 1);
             num *= (int) multiple;
             column += num;
