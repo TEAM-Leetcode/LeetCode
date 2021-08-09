22445f14ca0cac1df3a48de02a5dd393fb4a2ec0
JuYoung <ken44929@gmail.com>
2021-08-09 19:26:54 +0900

    Add: 1641 prob, Math

--- 주영/Array/1641. Count Sorted Vowel Strings.js
+++ 주영/Array/1641. Count Sorted Vowel Strings.js
@@ -3,19 +3,41 @@
  * @return {number}
  */
 var countVowelStrings = function (n) {
-  let temp = [];
+  let a = 1,
+    e = 1,
+    i = 1,
+    o = 1,
+    u = 1;
 
-  for (let i = 0; i < n + 1; i++) {
-    let binaryNum = i.toString(2);
-    let cnt = 0;
-    for (let j = 0; j < binaryNum.length; j++) {
-      if (binaryNum[j] === '1') cnt++;
-    }
-    temp.push(cnt);
+  for (let j = 1; j < n; j++) {
+    a = a + e + i + o + u;
+    e = e + i + o + u;
+    i = i + o + u;
+    o = o + u;
   }
-
-  console.log(temp);
+  return a + e + i + o + u;
 };
+// 끝에 나오는 문자
+
+// 1개일 때
+// a e i o u -> 5
+
+// 2개일 때
+// a - 5개 = 1 + 1 + 1 + 1 + 1
+// e - 4개 = 1 + 1 + 1 + 1
+// i - 3개 = 1 + 1 + 1
+// o - 2개 = 1 + 1
+// u - 1개 = 1
+
+// 3개일 때
+// a - 15 = 5 + 4 + 3 + 2 + 1
+// e - 11 = 4 + 3 + 2 + 1
+// i - 7 = 3 + 2 + 1
+// o - 3 = 2 + 1
+// u - 1
+
+// 4개일 때
+// 이 패턴이 반복된다.
 
 let n = 2;
 countVowelStrings(n);
