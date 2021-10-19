1ffe108787e80e68c96c34e7ad0789d7d637e1e1
JuYoung <ken44929@gmail.com>
2021-10-19 16:26:36 +0900

    Add prob 48

--- 주영/Any/48. Rotate Image.js
+++ 주영/Any/48. Rotate Image.js
@@ -1,4 +1,20 @@
-const rotate = (matrix) => {
-  matrix = matrix.reverse();
-  
+var rotate = function (matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = i + 1; j < matrix[i].length; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i].reverse();
+  }
 };
+
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+rotate(matrix);
