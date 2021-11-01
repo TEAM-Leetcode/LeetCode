469febfc53464f172bf5c0fd818e7a18e3831a0c
JuYoung <ken44929@gmail.com>
2021-11-01 15:12:17 +0900

    Retry prob 78

--- 주영/BackTracking/78. Subsets.js
+++ 주영/BackTracking/78. Subsets.js
@@ -1,18 +1,26 @@
+// 해설: https://velog.io/@ken1204/78.-Subsets
+
 var subsets = function (nums) {
   let result = [[]];
 
-  function backtrack(first, stack) {
+  function backtrack(first, current) {
+    // we iterate over the indexes i from 'first' to the length
+    //of the entire sequence 'nums'
     for (let i = first; i < nums.length; i++) {
-      stack.push(nums[i]);
-      result.push([...stack]);
-      backtrack(i + 1, stack);
-      stack.pop();
+      current.push(nums[i]);
+
+      // use distructure operator to clone 'current' value and save to 'result'
+      result.push([...current]);
+
+      // generate all other subsets for the current subset.
+      // increasing the position by one to avoid duplicates in 'result'
+      backtrack(i + 1, current);
+
+      // BACKTRACK.
+      current.pop();
     }
   }
 
   backtrack(0, []);
   return result;
 };
-
-let nums = [1, 2, 3];
-console.log(subsets(nums));
